3521a94e659991c26b693bb98b99a398f78e08a0
Nguyễn Đăng Mạnh <changomanh13@gmail.com>
2018-11-14 14:29:13 +0700

    tuan 10

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,96 @@
 package week10;
 
+import week9.Utils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+
+    public String getCodeFunction(String code, int startFunction) {
+        int countOpen = 0;
+        int countClose = 0;
+        int length = code.length();
+
+        for (int i = startFunction; i < length; i++) {
+            if (code.charAt(i) == '{') countOpen++;
+            if (code.charAt(i) == '}') countClose++;
+
+            if (countOpen == countClose && countOpen != 0) {
+                return code.substring(startFunction, i + 1);
+            }
+        }
+
+        return "";
+    }
+
+    public String getSourceCode(File path) {
+        String code = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String temp;
+            boolean isComment = false;
+            while ((temp = bufferedReader.readLine()) != null) {
+                if (temp.contains("/*")) {
+                    isComment = true;
+                    code += temp.substring(0, temp.indexOf("/*")) + "\n";
+                }
+
+                int indexComment = temp.indexOf("//");
+                if (indexComment != -1) {
+                    code += temp.substring(0, indexComment) + "\n";
+                } else {
+                    if (!isComment) code += temp + "\n";
+                }
+
+                if (temp.contains("*/")) {
+                    isComment = false;
+                    code += temp.substring(temp.indexOf("*/") + 2) + "\n";
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e) {
+            System.out.println("Error read file.");
+        }
+
+        return code;
+    }
+
+    public List<String> getAllFunctions(File path) {
+        List<String> result = new ArrayList<>();
+
+        String code = getSourceCode(path);
+        int pos = code.indexOf("public static");
+
+        while (pos != -1) {
+            String func = getCodeFunction(code, pos);
+            result.add(func);
+            pos = code.indexOf("public static", pos + 1);
+        }
+
+        return result;
+    }
+    public String findFunctionByName(String name){
+        Task1 task1 = new Task1();
+        List<String> findFuctions =  task1.getAllFunctions(new File("C:\\Users\\DELL\\IdeaProjects\\oop2018\\src\\week9\\Utils.java"));
+        String Name = name.substring(0,name.indexOf("("));
+        for(int i=0; i<findFuctions.size() ;i++){
+            if(findFuctions.get(i).indexOf(Name)!=-1){
+                return findFuctions.get(i);
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        List<String> funcs = task1.getAllFunctions(new File("C:\\Users\\DELL\\IdeaProjects\\oop2018\\src\\week9\\Utils.java"));
+
+        for (String func : funcs) {
+            Utils.NewContentToFile("C:\\Users\\DELL\\IdeaProjects\\oop2018\\src\\week10\\test.txt", func);
+        }
+
+        System.out.println(task1.findFunctionByName("writeContentToFile(String,String)"));
+    }
 }
--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -3,52 +3,51 @@ import java.io.*;
 
 public class Utils {
     //phương thức đọc vào 1 xâu ký tự từ file
-    public static String readContentFormFile(String path){
+    public static String readContentFormFile(String path) {
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
-            String result="";
+            String result = "";
             int i = 0;
-            while((i=fileInputStream.read())!=-1){
-                result+=(char)i;
+            while ((i = fileInputStream.read()) != -1) {
+                result += (char) i;
             }
             fileInputStream.close();
             return result;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
     //phương thức ghi một xâu ký tự mới vào file, nếu file đó đã có nội dung thì ta xóa nội dung cũ đi vào thêm nội dung mới vào
-    public static void writeContentToFile(String path, String newContent){
-        try{
+    public static void writeContentToFile(String path, String newContent) {
+        try {
             FileOutputStream fileOutputStream = new FileOutputStream(path);
             byte[] temp = newContent.getBytes();
             fileOutputStream.write(temp);
             fileOutputStream.close();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-     // phương thức ghi vào thêm vào file nội dung mới mà không xóa đi nội dung cũ
-    public static void NewContentToFile(String path,String newContent){
-        try{
-            FileWriter fileWriter = new FileWriter(path,true);
+
+
+    // phương thức ghi vào thêm vào file nội dung mới mà không xóa đi nội dung cũ
+    public static void NewContentToFile(String path, String newContent) {
+        try {
+            FileWriter fileWriter = new FileWriter(path, true);
             fileWriter.write("\n");
             fileWriter.write(newContent);
             fileWriter.close();
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     // phương thức tìm kiếm một file trong một thư mục
-    public static File findFileByName(String folderPath, String fileName){
-        try{
+    public static File findFileByName(String folderPath, String fileName) {
+        try {
             File dic = new File(folderPath);
             File[] arrFile = dic.listFiles(new FilenameFilter() {
                 @Override
@@ -57,20 +56,20 @@ public class Utils {
                 }
             });
             return arrFile[0];
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         String newString = Utils.readContentFormFile("src/week9/file.txt");
         System.out.println(newString);
         String newContent = "Ghi them vao khi da xoa noi dung cu";
-        Utils.writeContentToFile("src/week9/file.txt",newContent);
-        Utils.NewContentToFile("src/week9/file.txt","ghi them vao khong can xoa noi dung cu");
-        File searchFile = Utils.findFileByName("src/week9/","file.txt");
-        if(searchFile!=null)    System.out.println("File is exist");
+        Utils.writeContentToFile("src/week9/file.txt", newContent);
+        Utils.NewContentToFile("src/week9/file.txt", "ghi them vao khong can xoa noi dung cu");
+        File searchFile = Utils.findFileByName("src/week9/", "file.txt");
+        if (searchFile != null) System.out.println("File is exist");
         else System.out.println("File is not exist");
     }
 }
