e98ab25e4c2fd88332e858096761341cd4dfe2b4
Nguyễn Đăng Mạnh <changomanh13@gmail.com>
2018-11-21 13:45:23 +0700

    tuan 11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,31 @@
 package week11;
-
 public class Task1 {
+    public static <T extends Comparable> void sortArr(T[]a) {
+
+        int n = a.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (a[j+1].compareTo(a[j])<=0)
+                {
+                    T temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+    }
+
+    public static void main(String[] args) {
+        Task1 hihi = new Task1();
+        Character b[] = {'a','c','b','v'};
+        Integer a[] = {9,8,7,6,5,4,2,1};
+        hihi.<Integer>sortArr(a);
+        hihi.<Character>sortArr(b);
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " " );
+        }
+        System.out.println("\n");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+
+        }
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,34 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public static <T extends Comparable> T max(ArrayList<T> a) {
+        if(a == null || a.size() == 0) return null;
+        T max = a.get(0);
+        for (int i = 1; i< a.size(); i++){
+            if(max.compareTo(a.get(i))< 0)
+                max = a.get(i);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Double> b = new ArrayList<>();
+       // Integer[] a = {-5,6,-8,0,1,9,8,7,5,4};
+        //Double[] b = {-1.1,5.2,-11.5,1222.1,-2.5};
+        a.add(4);
+        a.add(5);
+        a.add(-6);
+        a.add(1000);
+        b.add(-1.1);
+        b.add(-1000.2);
+        b.add(3.5);
+        b.add(1.7);
+        int max = max(a);
+        double max1 = max(b);
+        System.out.println(max);
+        System.out.println(max1);
+    }
 }
